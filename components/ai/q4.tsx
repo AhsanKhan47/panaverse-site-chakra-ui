551c8d4073db407b80deeebd1eb6a3ca311566d7
Ahsankhan47 <ahsankhanafridi2000@gmail.com>
2023-03-07 01:02:32 +0500

    formating

--- components/ai/q4.tsx
+++ components/ai/q4.tsx
@@ -1,20 +1,13 @@
 import React from "react";
 import {
-  useColorMode,
-  Switch,
   Flex,
   Button,
-  IconButton,
   Box,
   Link,
   Text,
-  Center,
-  Spacer,
   Heading,
   ListItem,
   OrderedList,
-  Image,
-  Container,
 } from "@chakra-ui/react";
 export default function Q4() {
   return (
